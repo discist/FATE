28a228b181267504f4439809d29719f9c3874467
kukupie123 <kukuborom@gmail.com>
2022-03-11 18:49:24 +0530

    Created backend service

---
+++ frontend/src/backendServices/AuthService.js
@@ -0,0 +1,25 @@
+﻿import {auth} from "../firebase";
+import {signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+
+
+const provider = new GoogleAuthProvider();
+
+
+function showGoogleSigninPop() {
+    console.log("Show Google Sign in popup called")
+
+    signInWithPopup(auth, provider).then((result) => {
+        //Gives us the Google Access Token. You can use it to access the Google API
+        const credential = GoogleAuthProvider.credentialFromError(result);
+
+        const token = credential.accessToken;
+
+        const user = result.user;
+
+        console.log(result)
+
+    }).catch((error) => {
+        //Handling errors
+        console.log(error.message + " Code is : " + error.code)
+    });
+}
