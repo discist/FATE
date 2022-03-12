8b91dca2d4f982cd8da990da075bc074afe57c5d
akashdusky <discister@gmail.com>
2022-03-12 10:12:06 +0530

    dw
    dw

--- frontend/.svelte-kit/output/server/nodes/3.js
+++ frontend/.svelte-kit/output/server/nodes/3.js
@@ -3,4 +3,4 @@ import * as module from '../entries/pages/editprofile2.svelte.js';
 export { module };
 export const entry = 'pages/editprofile2.svelte-4a1f0d26.js';
 export const js = ["pages/editprofile2.svelte-4a1f0d26.js","chunks/vendor-afff52ae.js"];
-export const css = ["assets/pages/editprofile2.svelte-450b50cc.css"];
+export const css = ["assets/pages/home.svelte-853c449f.css"];
--- frontend/.svelte-kit/output/server/nodes/5.js
+++ frontend/.svelte-kit/output/server/nodes/5.js
@@ -3,4 +3,4 @@ import * as module from '../entries/pages/home.svelte.js';
 export { module };
 export const entry = 'pages/home.svelte-81872109.js';
 export const js = ["pages/home.svelte-81872109.js","chunks/vendor-afff52ae.js"];
-export const css = ["assets/pages/editprofile2.svelte-450b50cc.css"];
+export const css = ["assets/pages/home.svelte-853c449f.css"];
